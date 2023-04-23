1998775dae24b00ff5019047cc54f7b71516a129
0xguava <demo.lappy@protonmail.com>
2023-04-23 15:20:10 +0530

    files cleaned

--- config.def.h
+++ config.def.h
@@ -5,13 +5,8 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "JetBrains Mono:size=11" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-//static const char *colors[SchemeLast][2] = {
-//	/*     fg         bg       */
-//	[SchemeNorm] = { "#bbbbbb", "#222222" },
-//	[SchemeSel] = { "#eeeeee", "#005577" },
-//	[SchemeOut] = { "#000000", "#00ffff" },
-//};
 
+/*pywal Colors*/
 #include "/home/adi/.cache/wal/colors-wal-dmenu.h"
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
